f9d5fc772a8cfe56f2382702328c18a98bce4c4e
Devskiller <owl@devskiller.com>
2023-04-30 10:18:36 +0000

    Update src/proposals/ProposalList/ProposalList.jsx

--- src/proposals/ProposalList/ProposalList.jsx
+++ src/proposals/ProposalList/ProposalList.jsx
@@ -1,30 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
 import ProposalRow from "../ProposalRow";
 
 import "./ProposalList.css";
 
-const ProposalList = () => (
-    <ul data-testid="proposal-list" className="ProposalList">
-        {[].map((proposal) => (
-            <li
-                key={proposal.id}
-                className="ProposalList__item"
-            >
-                <Link
+import {getTalks} from './../../api/httpApi';
+
+const ProposalList = () => {
+    
+    const [propList, setPropList] = useState([]);
+
+    useEffect(() => {
+        getAllProposals()
+    },[])
+
+    const getAllProposals = async () => {
+        let data = await getTalks().then((data) => {
+            setPropList(data);
+        })
+    }
+
+    return (
+        <ul data-testid="proposal-list" className="ProposalList">
+            {propList && propList.map((proposal) => (
+                <li
                     key={proposal.id}
-                    className="ProposalList__item__link"
-                    to={`/proposals`}
+                    className="ProposalList__item"
                 >
-                    <ProposalRow
-                        proposal={proposal}
-                        onStatusUpdate={() => {}}
-                    />
-                </Link>
-            </li>
-        ))}
-    </ul>
-);
+                    <Link
+                        key={proposal.id}
+                        className="ProposalList__item__link"
+                        to={`/proposals`}
+                    >
+                        <ProposalRow
+                            proposal={proposal}
+                            onStatusUpdate={() => {}}
+                        />
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
 
 export default ProposalList;
